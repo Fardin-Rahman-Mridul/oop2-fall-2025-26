674fa41d7d77e2af3821e6d9bb9c0d86aa8c5366
Fardin <f.mridul10@gmail.com>
2025-12-15 21:31:16 +0600

    22-49521-3

---
+++ Lab/Indexersln/Indexer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRecordSystem
+{
+    interface IStudent
+    {
+        void DisplayInfo();
+    }
+
+    class Student : IStudent
+    {
+        private string name;
+        private int[] scores = new int[3];
+
+        public Student(string name)
+        {
+            this.name = name;
+        }
+
+        public int this[int index]
+        {
+            get { return scores[index]; }
+            set { scores[index] = value; }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Student Name: " + name);
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine("Score " + (i + 1) + ":" + scores[i]);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student("JAMIL");
+
+            stu[0] = 89;
+            stu[1] = 95;
+            stu[2] = 81;
+
+            stu.DisplayInfo();
+        }
+    }
+}
---
+++ Lab/Interfacesln/Interface/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+{
+    interface IPrintable
+    {
+        void Print();
+    }
+
+    interface ISerializable
+    {
+        void Save();
+    }
+
+    class Report : IPrintable, ISerializable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing the document");
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saving the document");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Report rep = new Report();
+            rep.Print();
+            rep.Save();
+        }
+    }
+}
